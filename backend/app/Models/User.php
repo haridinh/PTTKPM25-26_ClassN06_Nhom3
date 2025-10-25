109baa1630b7a7fcc5da9cadaaf7b55dfcbac130
haridinh <haidinh2034@gmail.com>
2025-10-25 19:22:06 +0700

    feat: add models for users, customers, roles, and permissions

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -3,14 +3,21 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\UserIsAdminEnum;
+use App\Enums\UserStatusEnum;
+use App\Traits\ActionButtonTrait;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, ActionButtonTrait, HasRoles, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +25,20 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'about',
         'email',
         'password',
+        'remember_token',
+        'image',
+        'last_login',
+        'last_logout',
+        'ip_address',
+        'status',
+        'is_admin',
+        'created_by',
+        'updated_by',
     ];
 
     /**
@@ -30,7 +48,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
+        'password_reset_token',
     ];
 
     /**
@@ -39,7 +57,50 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
+        'first_name'        => 'string',
+        'last_name'         => 'string',
+        'about'             => 'string',
+        'email'             => 'string',
+        'password'          => 'hashed',
+        'image'             => 'string',
+        'last_login'        => 'datetime',
+        'last_logout'       => 'datetime',
+        'ip_address'        => 'string',
+        'status'            => UserStatusEnum::class,
+        'is_admin'          => UserIsAdminEnum::class,
+        'created_by'        => 'integer',
+        'updated_by'        => 'integer',
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
+
+    /**
+     * Get Logo url
+     *
+     * @return string|null
+     */
+    public function getImageUrlAttribute(): ?string
+    {
+        return $this->image ? storage_asset($this->image) : null;
+    }
+
+    /**
+     * Name attribute
+     *
+     * @return void
+     */
+    public function getNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+
+    /**
+     *  creator info
+     *
+     * @return BelongsTo
+     */
+    public function articleLangData(): BelongsTo
+    {
+        return $this->belongsTo(ArticleLangData::class, 'created_by', 'id');
+    }
+
 }
