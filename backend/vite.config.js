93610f410b387a0715240c231ec50371fc7c1d91
haridinh <haidinh2034@gmail.com>
2025-10-25 17:49:33 +0700

    chore: setup package backend

--- backend/vite.config.js
+++ backend/vite.config.js
@@ -1,11 +1,17 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+export default defineConfig(() => {
+    dotenv.config();
+    return {
+        plugins: [
+            laravel({
+                input: ["resources/sass/app.scss", "resources/js/app.js"],
+                refresh: true,
+            }),
+            react(),
+        ],
+    };
 });
