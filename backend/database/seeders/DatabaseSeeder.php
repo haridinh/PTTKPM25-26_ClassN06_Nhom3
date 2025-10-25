8e11b87b098d79a5b05531b09fcfbb1714e52e38
haridinh <haidinh2034@gmail.com>
2025-10-25 20:53:35 +0700

    chore: add database seeders

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(UserSeeder::class);
+        $this->call(AcceptCurrencySeeder::class);
+        $this->call(ArticleSeeder::class);
+        $this->call(ArticleDataSeeder::class);
+        $this->call(LanguageSeeder::class);
+        $this->call(SettingSeeder::class);
+        $this->call(ArticleLangDataSeeder::class);
+        $this->call(PlanTimeSeeder::class);
+        $this->call(PackageSeeder::class);
+        $this->call(PermissionGroup::class);
+        $this->call(TeamMemberSeeder::class);
+        $this->call(PermissionSeeder::class);
+        $this->call(UserLogsSeeder::class);
+        $this->call(NotificationSetupSeeder::class);
+        $this->call(CreatePermissionSeeder::class);
     }
 }
